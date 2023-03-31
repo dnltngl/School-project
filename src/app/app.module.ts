6cd98492ec6259ce7bccdfc52d46f3abef7ec211
DnlTngl <99706377+dnltngl@users.noreply.github.com>
2023-03-31 23:10:56 +0800

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
-import { AchievementsComponent } from './achievements/achievements.component';
-import { ReferencesComponent } from './references/references.component';
-import { SkillsReferencesComponent } from './skills-references/skills-references.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { TableComponent } from './table/table.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import {MatTableModule} from '@angular/material/table';
+import { PanelComponent } from './panel/panel.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    EducationComponent,
-    SkillsComponent,
-    AchievementsComponent,
-    ReferencesComponent,
-    SkillsReferencesComponent
+    NavbarComponent,
+    FormComponent,
+    TableComponent,
+    ButtonsComponent,
+    PanelComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatTableModule,
+    MatExpansionModule,
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
